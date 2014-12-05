961585faa1f321df0a7d9aee50ad5fb4aa0882aa
Jeremy Pry <JPry@users.noreply.github.com>
2014-12-05 12:42:55 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ if [ ! hash brew 2>/dev/null]; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-
+# Ensure homebrew packages are installed
+source .brew
