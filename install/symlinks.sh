0b0f6e2964fea472cbe37a247637fc47ce3c3175
Jeremy Pry <JPry@users.noreply.github.com>
2015-01-23 17:54:28 -0500

    Create symlinks.sh

---
+++ install/symlinks.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+CWD="$(pwd)"
+DIR="$CWD/../"
+
+# Create symlinks
+for file in .{aliases,bash_profile,bash_prompt,colors,functions,gitconfig,path}; do
+    target="$HOME/$file"
+    if [[ -L $target ]]; then
+        echo "Link exists: $target"
+    else
+        echo "Creating new link: $DIR/$file -> $target"
+        ln -s $CWD/$file $target;
+    fi
+done
+unset file
+unset target
