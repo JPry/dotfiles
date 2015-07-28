301b039b6b332bd87c4b6b8497c99c26fd805a4d
Jeremy Pry <JPry@users.noreply.github.com>
2015-07-28 11:56:41 -0400

    Handle existing dotfiles

--- install/symlinks.sh
+++ install/symlinks.sh
@@ -9,6 +9,11 @@ for file in .{aliases,bash_profile,bash_prompt,colors,functions,gitconfig,path,z
     if [[ -L $target ]]; then
         echo "Link exists: $target"
     else
+        if [[ -e $target ]]; then
+            echo "File exists: $target"
+            echo "Moving $target to $target.old ..."
+            mv $target $target.old
+        fi
         echo "Creating new link: $DIR/$file -> $target"
         ln -s $CWD/$file $target;
     fi
